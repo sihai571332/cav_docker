f08c39a48fb69ee0d79d10d22c42305dbb214618
sihai571332 <sihai571332@gmail.com>
2019-01-22 15:27:07 -0600

    Add files via upload

---
+++ try.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+docker build -t cav/hello . 
+cd .. 
+ls
+if [ -d "shared_folder" ] 
+then
+  rm -r shared_folder
+fi
+mkdir shared_folder 
+cd shared_folder 
+docker run -p 9000:9000 -p 3000:3000 -v "$(pwd):/mydata" cav/hello /mydata
